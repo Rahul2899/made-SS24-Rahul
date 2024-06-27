af1cecd951d2ba4bb89fa18ef2f2348ef07eb2f2
Rahul2899 <53967666+Rahul2899@users.noreply.github.com>
2024-06-27 13:42:51 +0200

    git cache clear

--- project/Automated_test.sh
+++
@@ -1,9 +0,0 @@
-echo "Running Tests.."
-
-python ./Automated_test.py
-
-# Check the return value of the test script
-if [ $? -ne 0 ]; then
-    echo "System Test failed."
-    exit 1
-fi
--- project/datapipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install necessary Python packages
-pip install kaggle requests  pandas  pysqlite3 
-
-# Run the Python script
-python datapipeline.py
