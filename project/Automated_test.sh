0a68f1fc26b45ba95f7b200e432531685c1dd52c
Rahul2899 <53967666+Rahul2899@users.noreply.github.com>
2024-06-27 06:54:21 +0200

    changes in sh file

--- project/Automated_test.sh
+++ project/Automated_test.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+# Removing Existing Files
+echo "Starting Tests"
+echo "Installing pre-requisit lib"
+pip install pandas pysqlite3
 
-# Navigate to the project directory
-cd "$(dirname "$0")"
+echo "------------------------"
 
-python datapipeline.py
+echo "running system test."
+python ./Automated_test.py
 
-pytest Automated_test.py
-
-# Capture the exit code of pytest
-exit_code=$?
-
-exit $exit_code
+# Check the return value of system_test.sh
+if [ $? -ne 0 ]; then
+    echo "System Test failed."
+    exit 1
+fi
